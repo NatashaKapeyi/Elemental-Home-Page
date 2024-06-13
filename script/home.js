a55f6ce24871cf3cf78660890b5d66f1134cfa99
NatashaKapeyi <natashazikhona7@gmail>
2024-06-13 15:44:40 +0200

    slide almost completed

--- script/home.js
+++ script/home.js
@@ -1,26 +1,53 @@
-var slideIndex = 1;
-showDivs(slideIndex);
+$(document).ready(function() {
+    // Initialize the slide index to the first slide
+    let slideIndex = 1;
+    showDivs(slideIndex); // Display the first slide
 
-function plusDivs(n) {
-    showDivs(slideIndex += n);
-}
-
-function currentDiv(n) {
-    showDivs(slideIndex = n);
-}
-
-function showDivs(n) {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dot");
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
+    // Function to increment the slide index and show the corresponding slide
+    function plusDivs(n) {
+        showDivs(slideIndex += n);
     }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
+
+    // Function to set the slide index to the specific slide and show it
+    function currentDiv(n) {
+        showDivs(slideIndex = n);
     }
-    slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active";
-}
+
+    // Function to show the slide corresponding to the current slide index
+    function showDivs(n) {
+        // Get all the slides and dots
+        const slides = $(".mySlides");
+        const dots = $(".dot");
+
+        // If the slide index is greater than the number of slides, reset to the first slide
+        if (n > slides.length) { slideIndex = 1; }
+
+        // If the slide index is less than 1, set to the last slide
+        if (n < 1) { slideIndex = slides.length; }
+
+        // Hide all slides
+        slides.css("display", "none");
+
+        // Remove the active class from all dots
+        dots.removeClass("active");
+
+        // Display the current slide
+        slides.eq(slideIndex - 1).css("display", "block");
+
+        // Add the active class to the current dot
+        dots.eq(slideIndex - 1).addClass("active");
+    }
+
+    // Event listener for the next button to go to the next slide
+    $(".next").click(function() {
+        plusDivs(1);
+    });
+
+    // Event listener for the dots to go to the corresponding slide
+    $(".dot").click(function() {
+        // Get the index of the clicked dot and set the current slide to that index
+        const index = $(".dot").index(this) + 1;
+        currentDiv(index);
+    });
+});
+
